91f2527d6946ab64850d853683014d8a0f2718af
Vinicius Cruz <kruzvinicius@proton.me>
2026-03-19 11:17:57 -0300

    feat: Implement initial LIMS backend with sample management, audit logging, and test result capabilities.

--- lims-backend/src/main/java/com/kruzvinicius/limsbackend/LimsBackendApplication.java
+++ lims-backend/src/main/java/com/kruzvinicius/limsbackend/LimsBackendApplication.java
@@ -3,6 +3,8 @@ package com.kruzvinicius.limsbackend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+
 @SpringBootApplication
 public class LimsBackendApplication {
     public static void main(String[] args) {
